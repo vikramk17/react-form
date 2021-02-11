ccd2f8c59cfd1389add2dc3ba6278f0d926310da
vikramk17 <vickramskadam@gmail.com>
2021-02-11 15:38:34 +0530

    add navigation bar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div>
+      <header className="header">
+        <ul className="main-nav">
+          <li>
+            <a href="null">Home</a>
+          </li>
+          <li>
+            <a href="null">About</a>
+          </li>
+          <li>
+            <a href="null">Portfolio</a>
+          </li>
+          <li>
+            <a href="null">Contact</a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
